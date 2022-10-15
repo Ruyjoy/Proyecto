5843aa15e825478d0036123ecf7967a89453151a
Acharcitox <madarola@hotmail.com>
2022-10-15 19:40:51 -0300

    Html donaciones

--- View/donaciones.php
+++ View/donaciones.php
@@ -7,16 +7,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="CSS/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>DONACIONES</title>
 </head>
 
 <body class = "cuerpo">
-    
-                                
-    <div class="form-group d-flex align-items-center justify-content-between mt-4 mb-0">
-        <button name='enviar' type="submit" class="btn btn-primary btn-block btn-large">Doname un pesito</button>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-5">
+                <div class="card shadow-lg border-0 rounded-lg mt-5">
+                    <form>
+                        <div class="form-group col-sm-6">
+                            <h5><label for="apellido">Monto</label></h5>
+                            <br>
+                            <input type="double" class="form-control" id="monto" placeholder="Ingrese un monto">
+                        </div>
+
+                        <br><br>
+
+                        <div class="form-row">
+                            <div class="form-group col-sm-6">
+                                <h5><label for="pago">Medio de Pago</label></h5>
+                                <br>
+                                <select id="pago" class="form-select">
+                                    <option selected>Mercado Pago</option>
+                                    <option>PayPal</option>
+                                    <option>Tarjeta de Crédito/Débito</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <br><br>
+                        <div class="form-group">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="" id="gridCheck">
+                                <label class="form-check-label" form="gridCheck">
+                                    Quiero recibir información sobre ONGs.
+                                </label>
+                            </div>
+                        </div>
+                        
+                        <br><br>
+
+                        <button type="submit" class="btn btn-primary">DONAR</button>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
     </div>
-                            
+
 </body>
 
 </html>
