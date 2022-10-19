de65e5d1c24d2afd94ee4bc38c333cc0c11944a4
Jose Luis Ferrer <Joe@DESKTOP.com>
2022-10-19 19:24:25 -0300

    Cambios con la base de datos

--- Controller/C_registroUsuario.php
+++ Controller/C_registroUsuario.php
@@ -1,177 +1,103 @@
-<?php 
- /*include ("../Model/Conexion.php");
- $db = new Conexion();
- $con = $db->conectar(); 
+<?php
 
- if(isset($_POST['crear'])){
-    if(empty($_POST['cedula']) or empty($_POST['nombre']) or empty($_POST['nombre']) or empty($_POST['nombre']) or empty($_POST['nombre'])){
-        echo "Uno de los campos está vacio";
-    }else{
-    
-        $cedula = $_POST['cedula'];
-        $nombre = $_POST['nombre'];
-        $apellido = $_POST['ape'];
-        $direccion = $_POST['dir'];
-        $telefono = $_POST['tel'];
+//require_once ("/laragon/www/Proyecto/Model/Conexion.php");
 
-        $consulta = "INSERT INTO cliente(cli_ci, cli_nombre, cli_apellido, cli_direccion, cli_telefono)VALUES ($cedula, '$nombre',  '$apellido',  '$direccion', $telefono)";
-        $resultado = mysqli_query($con, $consulta);
 
-        //Si existe en base de datos -------
-     
-        if($resultado == true) {
-            echo "Correcto";
-            //echo "<script>alert('Se a Agregado correcatamente, actualice la p\u00E1gina para ver los cambios'); window.location='lisadocliente.php'</script>";
-    
-        }else {
-            echo "error";
-        }
-   
-    }
-    
-    mysqli_close($con);
- }
+include("../Model/Conexion.php");
 
- include ("../Model/Conexion.php");
- 
- if(isset($_POST['crear'])){
-    if(empty($_POST['cedula']) or empty($_POST['nombre']) or empty($_POST['nombre']) or empty($_POST['nombre']) or empty($_POST['nombre'])){
-        echo "Uno de los campos está vacio";
-    }else{
-    
-        $cedula = $_POST['cedula'];
-        $nombreusuario = $_POST['nombreusuario'];
-        $nombre = $_POST['nombre'];
-        $pass = $_POST['pass'];
-        $telefono = $_POST['tel'];
-        $mail = $_POST['mail'];
 
-        $registro = new M_registroUsuario();
-        $registro->registra($cedula, $nombreusuario, $pass,  $nombre,  $telefono, $mail);
-    }
- }
+$db = new Conexion();
+$con = $db->conectar();
 
-?>*/
-
-    require_once ("/laragon/www/Proyecto/Model/Conexion.php");
-    include ("/laragon/www/Proyecto/View/RegistroUsuarios.php");
-
-    $db = new Conexion();
-    $conexion = $db->conectar();
-
-    // Almacena los datos
-    $nombre = $db->limpiar_cadena( $_POST['nombre'] );
-
-    $cedula = $db->limpiar_cadena( $_POST['cedula'] );
-
-    $usuario = $db->limpiar_cadena( $_POST['usuario'] );
-
-    $telefono = $db->limpiar_cadena( $_['tel'] );
-
-    $email = $db->limpiar_cadena( $_POST['email'] );
+// Almacena los datos
+if (isset($_POST['crear'])) {
+    $codigo = $db->limpiar_cadena($_POST['codigo']);
+    $nombre = $db->limpiar_cadena($_POST['nombre']);
+    $telefono = $db->limpiar_cadena($_POST['tel']);
+    $direccion = $db->limpiar_cadena($_POST['dir']);
+    $mail = $db->limpiar_cadena($_POST['email']);
 
     $rol = 2; //Variable que guarda el rol del usuario que se registra
 
-    $clave_1 = $db->limpiar_cadena( $_POST['pass'] );
-    $clave_2 = $db->limpiar_cadena( $_POST['pass2'] );
+    $clave_1 = $db->limpiar_cadena($_POST['pass']);
+    $clave_2 = $db->limpiar_cadena($_POST['pass2']);
 
     // Vereficamos los campos que debe ingresar el nuevo usuario
-    if ( $nombre == "" || $cedula == "" || $usuario == "" || $telefono == "" || $clave_1 == "" || $clave_2 == "" ) {
+    if ($nombre == "" || $codigo == "" || $direccion == "" || $telefono == "" || $clave_1 == "" || $clave_2 == "") {
 
-        echo "Ocurrió un error inesperado"."<br>"."Debes completar todos los campos requeridos";
+        echo "Ocurrió un error inesperado" . "<br>" . "Debes completar todos los campos requeridos";
         exit();
     }
 
     // Verificamos integridad de los datos
-    
-    if ( $db ->verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,70}", $nombre)) {
 
-        echo "Ocurrió un error inesperado"."<br>"."El nombre no coincide con el formato";
+    if ($db->verificar_datos("[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,70}", $nombre)) {
+
+        echo "Ocurrió un error inesperado" . "<br>" . "El nombre no coincide con el formato";
         exit();
-
     }
 
-    if ( $db->verificar_datos("[.- ]{7,100}", $cedula)) {
 
-        echo "Ocurrió un error inesperado"."<br>"."La cédula de identidad no coincide con el formato";
+
+    //Tenes que arreglar estos filtros y poner cual es el formato que necesitas 
+
+   /*if ($db->verificar_datos("{4,20}", $telefono)) {
+
+        echo "Ocurrió un error inesperado" . "<br>" . "El número de teléfono no coincide con el formato";
         exit();
-
     }
 
-    if ( $db->verificar_datos("[a-zA-Z0-9]{4,20}", $usuario)) {
+    if ($db->verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_1 ||
+        $db->verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_2))) {
 
-        echo "Ocurrió un error inesperado"."<br>"."Nombre de usuario no coincide con el formato";
+        echo "Ocurrió un error inesperado" . "<br>" . "La contraseña no coincide con el formato";
         exit();
+    }*/
 
-    }
 
-    if ( $db ->verificar_datos("{4,20}", $telefono)) {
 
-        echo "Ocurrió un error inesperado"."<br>"."El número de teléfono no coincide con el formato";
-        exit();
 
-    }
 
-    if ( $db ->verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_1 || 
-            $db ->verificar_datos("[a-zA-Z0-9$@.-]{7,100}", $clave_2)) ) {
 
-            echo "Ocurrió un error inesperado"."<br>"."La contraseña no coincide con el formato";
+    // Verificamos datos únicos  Falta mail
+
+    if ($codigo != "") {
+
+        $consulta = ("SELECT * FROM usuario WHERE codigo = '$codigo' ");
+        $check_user = mysqli_query($con, $consulta);
+
+
+        if ($fila = mysqli_fetch_assoc($check_user)) {
+
+            echo "Ocurrió un error inesperado" . "<br>" . "El usuario ya existe.";
             exit();
-
-    }
-    
-    // Verificamos datos únicos
-
-    if ( $email != "" || $cedula != "" ) {
-
-            $consulta = ("SELECT * FROM usuario WHERE cedula = $cedula AND mail = '$email'"); // cambiar cedula a varchar
-            $check_user = mysqli_query( $conexion, $consulta);
-        
-
-            if ( $check_user == true ) {
-                
-                echo "Ocurrió un error inesperado"."<br>"."El usuario ya existe.";
-                exit();
-
-            }
-
+        }
     }
 
     // verifica las contraseñas
-    if ( $clave_1 != $clave_2 ) {
+    if ($clave_1 != $clave_2) {
 
-        echo "Ocurrió un error inesperado"."<br>"."Las contraseñas no coinciden.";
+        echo "Ocurrió un error inesperado" . "<br>" . "Las contraseñas no coinciden.";
         exit();
 
     } else {
 
-        $clave = password_hash( $clave_1, PASSWORD_BCRYPT, ["cost" => 10]); // encripta la contraseña
+        $clave = password_hash($clave_1, PASSWORD_BCRYPT, ["cost" => 10]); // encripta la contraseña
 
     }
 
     // Guarda los datos
-    $guardar_usuario = $conexion;
-    $guardar_usuario = $guardar_usuario -> prepare( // método 'prepare' para evitar inyección sql
-        "INSERT INTO usuario(ci,nombre_usuario,pass,nombre,telefono,mail,rol) 
-        VALUES(:cedula,:usuario,:clave,:nombre,:telefono,:email,:rol)");  // marcadores :nombre 
-    
-    $marcadores = [
-        ":cedula" => $cedula,
-        ":usuario" => $usuario,
-        ":clave" => $clave,
-        ":nombre" => $nombre,
-        ":telefono" => $telefono,
-        ":mail" => $email,
-        "rol" => $rol
-    ];
-    // ejecuta la consulta
-    $guardar_usuario -> execute( $marcadores );
 
-    $guardar_usuario = null; // cierra la conexión a la BD
+    $consulta = "INSERT INTO usuario(codigo, pass, nombre, telefono, direccion, mail, rol)VALUES ('$codigo', '$clave', '$nombre', $telefono , '$direccion', '$mail', $rol)";
+    $resultado = mysqli_query($con, $consulta);
 
+    if ($resultado == true) {
+        echo "Correcto";
+        //echo "<script>alert('Se a Agregado correcatamente, actualice la p\u00E1gina para ver los cambios'); window.location='lisadocliente.php'</script>";
 
+    } else {
+        echo "error";
+    }
 
-
-
-
-
+    mysqli_close($con);
+}
--- View/RegistroUsuarios.php
+++ View/RegistroUsuarios.php
@@ -1,5 +1,6 @@
 <?php
- include ("/laragon/www/Proyecto/Controller/C_registroUsuario.php");
+//include ("/laragon/www/Proyecto/Controller/C_registroUsuario.php");
+include ("../Controller/C_registroUsuario.php");
 ?>
 
 <!DOCTYPE html>
@@ -20,79 +21,79 @@
 
 
 <body class="cuerpo">
-        <div id="layoutAuthentication">
-            <div id="layoutAuthentication_content">
-                <main>
-                    <div class="container">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-7">
-                                <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header">
-                                        <h3 class="text-center font-weight-light my-4">Crear Cuenta</h3>
-                                    </div>
-                                    <div class="card-body">
-                                        <form action="" method="POST" autocomplete="off" class="FormularioAjax">
-                                            <div class="form-row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="small mb-1">Nombre y Apellido</label>
-				  	                                    <input class="form-control py-2" type="text" name="nombre" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,70}" maxlength="70" placeholder="Ingrese su nombre y apellido" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Cédula de identidad</label>
-                                                        <input class="form-control py-2" name="cedula" pattern="[.- ]{7,100}" type="number" placeholder="Ingrese su número de cédula" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Nombre Usuario</label>
-                                                        <input class="form-control py-2" name="usuario" pattern="[a-zA-Z0-9]{4,20}" type="text" placeholder="Ingrese un nombre de usuario" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Telefono</label>
-                                                        <input class="form-control py-2" name="tel"pattern="{4,20}" type="text" placeholder="Ingrese un número de teléfono" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Correo Electrónico</label>
-                                                        <input class="form-control py-2" name="email" required="required" type="email" placeholder="Ingrese su correo electrónico" />
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Contraseña</label>
-                                                        <input class="form-control py-2" name="pass" pattern="[a-zA-Z0-9$@.-]{7,100}" type="password" placeholder="Ingrese una contraseña" required/>
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label class="small mb-1" for="inputLastName">Repetir Contraseña</label>
-                                                        <input class="form-control py-2" name="pass2" pattern="[a-zA-Z0-9$@.-]{7,100}" type="password" placeholder="Ingrese nuevamente la contraseña" required/>
-                                                    </div>
-                                                </div>
-
-                                            </div>
-                                            <br>
-                                            <p>
-                                                <button class="btn btn-primary btn-block" name="crear">Crear Cuenta</button>
-                                            </p>
-                                            
-                                        </form>
-                                    </div>
-
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-7">
+                            <div class="card shadow-lg border-0 rounded-lg mt-5">
+                                <div class="card-header">
+                                    <h3 class="text-center font-weight-light my-4">Crear Cuenta</h3>
                                 </div>
+                                <div class="card-body">
+                                    <form action="" method="POST" autocomplete="off" class="FormularioAjax">
+                                        <div class="form-row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Nombre Usuario / Codigo</label>
+                                                    <input class="form-control py-2" name="codigo" type="text" placeholder="Ingrese Nombre Usuario / Codigo" required />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1">Nombre Completo</label>
+                                                    <input class="form-control py-2" type="text" name="nombre" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{3,70}" maxlength="70" placeholder="Ingrese su nombre y apellido" required />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Telefono</label>
+                                                    <input class="form-control py-2" name="tel" pattern="{4,20}" type="text" placeholder="Ingrese un número de teléfono" required />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Direccion</label>
+                                                    <input class="form-control py-2" name="dir" required="required" type="text" placeholder="Ingrese su Direccion" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Correo Electrónico</label>
+                                                    <input class="form-control py-2" name="email" required="required" type="email" placeholder="Ingrese su correo electrónico" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Contraseña</label>
+                                                    <input class="form-control py-2" name="pass"  type="password" placeholder="Ingrese una contraseña" required />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="small mb-1" for="inputLastName">Repetir Contraseña</label>
+                                                    <input class="form-control py-2" name="pass2" type="password" placeholder="Ingrese nuevamente la contraseña" required />
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <br>
+                                        <p>
+                                            <button class="btn btn-primary btn-block" name="crear">Crear Cuenta</button>
+                                        </p>
+
+                                    </form>
+                                </div>
+
                             </div>
                         </div>
                     </div>
-                </main>
-            </div>
+                </div>
+            </main>
         </div>
-    
+    </div>
+
 
 
 </body>
