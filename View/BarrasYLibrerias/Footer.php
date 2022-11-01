51bb92467298297836c7b4bf8e766135bf6bb6c7
Jose Luis <ruyjoy@hotmail.com>
2022-11-01 15:30:41 -0300

    cambios de index

--- View/BarrasYLibrerias/Footer.php
+++ View/BarrasYLibrerias/Footer.php
@@ -1,104 +1,50 @@
+<!-- Footer Start -->
+<div class="container-fluid bg-dark text-secondary mt-5 pt-5">
+    <div class="row px-xl-5 pt-5">
+        <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
+            <h5 class="text-secondary text-uppercase mb-4">Acercate</h5>
+            <p class="mb-4">Puedes encontrarnos en</p>
+            <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Calle, Ciudad, País</p>
+            <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
+            <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+598 010 10101</p>
+        </div>
+        <div class="col-lg-8 col-md-12">
+            <div class="row">
+                <div class="col-md-4 mb-5">
+                    <h5 class="text-secondary text-uppercase mb-4">COMUNICATE</h5>
+                    <h6 class="text-secondary text-uppercase mt-4 mb-3">Síguenos en nuestras redes</h6>
+                    <div class="d-flex">
+                        <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
 
-  <!-- Footer Start -->
-  <div class="container-fluid bg-dark text-secondary mt-5 pt-5">
-      <div class="row px-xl-5 pt-5">
-          <div class="col-lg-4 col-md-12 mb-5 pr-3 pr-xl-5">
-              <h5 class="text-secondary text-uppercase mb-4">Get In Touch</h5>
-              <p class="mb-4">No dolore ipsum accusam no lorem. Invidunt sed clita kasd clita et et dolor sed dolor. Rebum tempor no vero est magna amet no</p>
-              <p class="mb-2"><i class="fa fa-map-marker-alt text-primary mr-3"></i>123 Street, New York, USA</p>
-              <p class="mb-2"><i class="fa fa-envelope text-primary mr-3"></i>info@example.com</p>
-              <p class="mb-0"><i class="fa fa-phone-alt text-primary mr-3"></i>+012 345 67890</p>
-          </div>
-          <div class="col-lg-8 col-md-12">
-              <div class="row">
-                  <div class="col-md-4 mb-5">
-                      <h5 class="text-secondary text-uppercase mb-4">Quick Shop</h5>
-                      <div class="d-flex flex-column justify-content-start">
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                          <a class="text-secondary" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
-                      </div>
-                  </div>
-                  <div class="col-md-4 mb-5">
-                      <h5 class="text-secondary text-uppercase mb-4">My Account</h5>
-                      <div class="d-flex flex-column justify-content-start">
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Home</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Our Shop</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shop Detail</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Shopping Cart</a>
-                          <a class="text-secondary mb-2" href="#"><i class="fa fa-angle-right mr-2"></i>Checkout</a>
-                          <a class="text-secondary" href="#"><i class="fa fa-angle-right mr-2"></i>Contact Us</a>
-                      </div>
-                  </div>
-                  <div class="col-md-4 mb-5">
-                      <h5 class="text-secondary text-uppercase mb-4">Newsletter</h5>
-                      <p>Duo stet tempor ipsum sit amet magna ipsum tempor est</p>
-                      <form action="">
-                          <div class="input-group">
-                              <input type="text" class="form-control" placeholder="Your Email Address">
-                              <div class="input-group-append">
-                                  <button class="btn btn-primary">Sign Up</button>
-                              </div>
-                          </div>
-                      </form>
-                      <h6 class="text-secondary text-uppercase mt-4 mb-3">Follow Us</h6>
-                      <div class="d-flex">
-                          <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-twitter"></i></a>
-                          <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-facebook-f"></i></a>
-                          <a class="btn btn-primary btn-square mr-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                          <a class="btn btn-primary btn-square" href="#"><i class="fab fa-instagram"></i></a>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-      <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
-          <div class="col-md-6 px-xl-0">
-              <p class="mb-md-0 text-center text-md-left text-secondary">
-                  &copy; <a class="text-primary" href="#">Domain</a>. All Rights Reserved. Designed
-                  by
-                  <a class="text-primary" href="https://htmlcodex.com">HTML Codex</a>
-              </p>
-          </div>
-          <div class="col-md-6 px-xl-0 text-center text-md-right">
-              <img class="img-fluid" src="img/payments.png" alt="">
-          </div>
-      </div>
-  </div>
-  <!-- Footer End -->
+                <div class="col-md-8 mb-5 ml-auto">
+                    <h5 class="text-secondary text-uppercase mb-4">¿Te interesa colaborar?</h5>
+                    <p>Sólo debes dar click en el botón DONAR y podrás elegir una ONG con la que colaborar.</p>
 
-  <div class="container">
-	      <div class="col-md-3 text-center">
-	         <!-- Footer logo -->
-	         <img src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/logof.png" alt="" class="center-block img-responsive">
-	      </div>
-	      <!-- /.col-md-4 -->
-	      <div class="col-md-3 text-center res-margin">
-	         <ul class="list-unstyled">
-	            <li><i class="fa fa-phone colorh"></i>(045) 2222 07 3129</li>
-	            <li><i class="fa fa-envelope colorh"></i><a href="mailto:hola@huellitasperdidas.org">hola@huellitasperdidas.org</a></li>
-	            <li><i class="fa fa-map-marker colorh"></i>Puebla, Puebla México</li>
-	         </ul>
-	      </div>
-	      <!-- /.col-md-4 -->
-	      <div class="col-md-3 text-center res-margin">
-	         <h5 class="colorh">Síguenos en Redes Sociales</h5>
-	         <!--Social icons -->
-	         <div class="social-media smaller">
-	            <a href="https://www.facebook.com/HuellitasPerdidasORG/" target="_blank"><i class="fa fa-facebook" style="color: #fff;"></i></a>
-	            <a href="https://www.instagram.com/huellitasperdidasorg/" target="_blank"><i class="fa fa-instagram" style="color: #fff;"></i></a>
-	            <a href="https://twitter.com/HuelliPerdiORG" target="_blank"><i class="fa fa-twitter" style="color: #fff;"></i></a>
-	            <a href="http://huellitasperdidas.business.site/" target="_blank"><i class="fa fa-google-plus" style="color: #fff;"></i></a>
-	         </div>
-	      </div>
-	      <!-- /.col-md-4 -->
-	      <div class="col-md-3 text-center res-margin">
-	         <!--Social icons -->
-	         <div class="social-media smaller">
-	            <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=B7A93D7BMBQJC" class="btn">Donaciones</a>
-	         </div>
-	      </div>
-	   </div>
+                    <div class="input-group">
+                        <div>
+                            <a href="../View/donaciones.php"><button class="btn btn-primary">Donaciones</button></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
+        <div class="col-md-6 px-xl-0">
+            <p class="mb-md-0 text-center text-md-left text-secondary">
+                &copy; <a class="text-primary" href="#">Uy</a>. Todos los derechos reservados. Diseñado por
+                <a class="text-primary" href="#">Paw Control</a>
+            </p>
+        </div>
+        <div class="col-md-6 px-xl-0 text-center text-md-right">
+            <img class="img-fluid" src="img/payments.png" alt="">
+        </div>
+    </div>
+</div>
+<!-- Footer End -->
