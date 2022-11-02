51384c548c51a9851842ce5d0f744c6d665a338c
Jose Luis <ruyjoy@hotmail.com>
2022-11-01 23:20:22 -0300

    Cambios Jose

--- View/Index.php
+++ View/Index.php
@@ -1,9 +1,3 @@
-<?php require_once "../Inc/session_start.php"; 
-      include "../Controller/C_login.php";
-      include "../Inc/script.php";
-?>
-
-
 <html lang="es">
 
 <head>
@@ -39,8 +33,8 @@
     <main>
         <!-- Carousel Start -->
         <div class="container-fluid mb-3">
-            <div class="row px-xl-5">
-                <div class="col-lg-8">
+            <div class="row px-xl-1">
+                <div class="col-lg-12">
                     <div id="header-carousel" class="carousel slide carousel-fade mb-30 mb-lg-0" data-ride="carousel">
                         <ol class="carousel-indicators">
                             <li data-target="#header-carousel" data-slide-to="0" class="active"></li>
@@ -49,83 +43,161 @@
                         </ol>
                         <div class="carousel-inner">
                             <div class="carousel-item position-relative active" style="height: 430px;">
-                                <img class="position-absolute w-100 h-100" src="../img/imgcar-1.png" style="object-fit: cover;">
+                                <img class="position-absolute w-100 h-100" src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/slide0.jpg" style="object-fit: cover;">
                                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                     <div class="p-3" style="max-width: 700px;">
-                                        <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">PetPoint</h1>
-                                        <!--<p class="mx-md-5 px-5 animate__animated animate__bounceIn"></p>-->
-                                        
+                                        <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">Pet Point</h1>
+                                        <div class="hidden-mobile">
+                                            <p class="lead">Estas son las mascotas que han sido rescatadas. Búscala en nuestro listado.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="carousel-item position-relative" style="height: 430px;">
-                                <img class="position-absolute w-100 h-100" src="../img/imgcar-2.png" style="object-fit: cover;">
+                                <img class="position-absolute w-100 h-100" src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/slide2.jpg" style="object-fit: cover;">
                                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                     <div class="p-3" style="max-width: 700px;">
-                                        <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown" color="green">PetPoint</h1>
-                                        
-                                        <!--<a class="btn btn-outline-light py-2 px-4 mt-3 animate__animated animate__fadeInUp" href="#">Shop Now</a>-->
+                                        <h1 class="slide__text-heading">¿Perdiste a tu mascota?</h1>
+                                        <div class="hidden-mobile">
+                                            <p class="lead">Estas son las mascotas que han sido rescatadas. Búscala en nuestro listado.</p>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                             <div class="carousel-item position-relative" style="height: 430px;">
-                                <img class="position-absolute w-100 h-100" src="../img/imgcar-3.png" style="object-fit: cover;">
+                                <img class="position-absolute w-100 h-100" src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/slide1.jpg" style="object-fit: cover;">
                                 <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                                     <div class="p-3" style="max-width: 700px;">
-                                        <h1 class="display-4 text-white mb-3 animate__animated animate__fadeInDown">PetPoint</h1>
-                                  
+
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4">
-                    <div class="product-offer mb-30" style="height: 200px;">
-                        <img class="img-fluid" src="../img/imgside1.png" alt="">
-                        <!--<div class="offer-text">
-                            <h6 class="text-white text-uppercase">Save 20%</h6>
-                            <h3 class="text-white mb-3">Special Offer</h3>
-                            <a href="" class="btn btn-primary">Shop Now</a>
-                        </div>-->
+            </div>
+        </div>
+        <!-- Carousel End -->
+    </main>
+
+
+    <!-- Sobre nosotros -->
+    <div class="card">
+        <blockquote class="blockquote mb-0 text-center">
+            <h2 style="font-size: 60px; font-family: 'Baloo Thambi', sans-serif; margin-bottom: 20px; margin-top: 0px; color: #108896; letter-spacing: -.025em; text-transform: uppercase;">¿Quienes Somos?</h2>
+            <h4>Pet Point</h4>
+        </blockquote>
+        <div class="card-body">
+            <div class="col-md-12 col-md-offset-1 text-center">
+                <p title="Título fuente" style="font-size: 17px; line-height: 34px;">Somos la plataforma donde encontrarás todos los
+                    servicios que te ofrece PetPoint. Trabajamos junto a distintas ONGs
+                    que se dedican al cuidado animal. Puedes conocer su labor en nuestra
+                    plataforma y realizar donaciones. Nos encanta lo que hacemos. Nos
+                    motivan nuestros clientes, la atención a las mascotas y la
+                    cooperación. Queremos que seas parte del cambio. Te ofrecemos un espacio donde
+                    podrás publicar mascotas perdidas y ver mascotas que están en adopción.</p>
+            </div>
+        </div>
+
+        <div class="container-fluid pt-5 pb-3">
+            <div class="row px-xl-5">
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                    <div class="product-item bg-light mb-4">
+                        <div class="product-img position-relative overflow-hidden">
+                            <img src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/service1.jpg"  width="300" height="280" alt="">
+                        </div>
+                        <div class="text-center py-4">
+                            <a class="h6 text-decoration-none text-truncate" href="../View/Mascota_Adopcion.php">Mascotas en adopción</a>
+                            <div class="d-flex align-items-center justify-content-center mt-2">
+                                <h9>Puedes encontar todas las mascotas en adopción por las Ong's.</h9>
+                            </div>
+                        </div>
                     </div>
-                    <div class="product-offer mb-30" style="height: 200px;">
-                        <img class="img-fluid" src="../img/imgside2.png" alt="">
-                        <!--<div class="offer-text">
-                            <h6 class="text-white text-uppercase">Save 20%</h6>
-                            <h3 class="text-white mb-3">Special Offer</h3>
-                            <a href="" class="btn btn-primary">Shop Now</a>
-                        </div>-->
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                    <div class="product-item bg-light mb-4">
+                        <div class="product-img position-relative overflow-hidden">
+                            <img src="https://huellitasperdidas.org/wp-content/themes/Idwasoft/img/service2.jpg"  width="300" height="280" alt="">
+                        </div>
+                        <div class="text-center py-4">
+                            <a class="h6 text-decoration-none text-truncate" href="../View/MascotaPerdidas.php">Mascotas perdidas</a>
+                            <div class="d-flex align-items-center justify-content-center mt-2">
+                                <h9>Aqui puede ver todas las mascotas perdidas.</h9>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                    <div class="product-item bg-light mb-4">
+                        <div class="product-img position-relative overflow-hidden">
+                        <img src="../img/imgside2.png"  alt=""  width="300" height="280">
+                        </div>
+                        <div class="text-center py-4">
+                            <a class="h6 text-decoration-none text-truncate" href="">Consultas</a>
+                            <div class="d-flex align-items-center justify-content-center mt-2">
+                            <h9>Podes encontrar todos los productos en nuestra veterinaria</h9>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
+                    <div class="product-item bg-light mb-4">
+                        <div class="product-img position-relative overflow-hidden">
+                            <img src="../img/imgside2.png" width="500" height="400" class="img-fluid w-100" alt="" >
+                        </div>
+                        <div class="text-center py-4">
+                            <a class="h6 text-decoration-none text-truncate" href="">Nuestros Productos</a>
+                            <div class="d-flex align-items-center justify-content-center mt-2">
+                                <h9>Podes encontrar todos los productos en nuestra veterinaria</h9>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!-- Carousel End -->
-        
-      
-    </main>
 
-    <main>
-      <!-- Quiénes somos -->
-      <div class="card">
-        <div class="card-header">
-          <blockquote class="blockquote mb-0 text-center">
-              <p>Sobre nosotros</p>
-          </blockquote>
-        </div>
+        <!-- Quiénes somos -->
+        <blockquote class="blockquote mb-0 text-center">
+            <h2 style="font-size: 60px; font-family: 'Baloo Thambi', sans-serif; margin-bottom: 20px; margin-top: 0px; color: #108896; letter-spacing: -.025em; text-transform: uppercase;">Sobre nosotros</h2>
+            <h4>Pet Point</h4>
+        </blockquote>
         <div class="card-body">
-          <figcaption class="blockquote-footer">
-            <cite title="Título fuente">Somos la plataforma donde encontrarás todos los
-            servicios que te ofrece PetPoint. Trabajamos junto a distintas ONGs
-            que se dedican al cuidado animal. Puedes conocer su labor en nuestra
-            plataforma y realizar donaciones. Nos encanta lo que hacemos. Nos
-            motivan nuestros clientes, la atención a las mascotas y la
-            cooperación. Queremos que seas parte del cambio. Te ofrecemos un espacio donde 
-            podrás publicar mascotas perdidas y ver mascotas que están en adopción.</cite>
-          </figcaption>
+            <div class="col-md-12 col-md-offset-1 text-center">
+                <p title="Título fuente" style="font-size: 17px; line-height: 34px;">Somos la plataforma donde encontrarás todos los
+                    servicios que te ofrece PetPoint. Trabajamos junto a distintas ONGs
+                    que se dedican al cuidado animal. Puedes conocer su labor en nuestra
+                    plataforma y realizar donaciones. Nos encanta lo que hacemos. Nos
+                    motivan nuestros clientes, la atención a las mascotas y la
+                    cooperación. Queremos que seas parte del cambio. Te ofrecemos un espacio donde
+                    podrás publicar mascotas perdidas y ver mascotas que están en adopción.</p>
+            </div>
         </div>
-      </div>
-    </main> 
+
+
+        <div class="row px-xl-5">
+            <div class="col-lg-4">
+                <div class="product-offer mb-30" style="height: 200px;">
+                    <img class="img-fluid" src="../img/imgcar-1.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="product-offer mb-30" style="height: 200px;">
+                    <img class="img-fluid" src="../img/imgcar-3.png" alt="">
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="product-offer mb-30" style="height: 200px;">
+                    <img class="img-fluid" src="../img/imgcar-2.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
+
+
+
 
     <!-- Footer Start -->
     <?php
