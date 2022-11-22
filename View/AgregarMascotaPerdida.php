f6b8a36b2bdb6eb49691839924125b22cba6d62a
Jose Luis Ferrer <Joe@DESKTOP.com>
2022-11-22 16:36:56 -0300

    Completito

--- View/AgregarMascotaPerdida.php
+++ View/AgregarMascotaPerdida.php
@@ -43,18 +43,6 @@
                 <span class="h1 text-uppercase text-dark bg-primary px-2 ml-n1">Point</span>
             </a>
         </div>
-        <div class="col-lg-4 col-6 text-left">
-            <form action="">
-                <div class="input-group">
-                    <input type="text" class="form-control" placeholder="Search for products">
-                    <div class="input-group-append">
-                        <span class="input-group-text bg-transparent text-primary">
-                            <i class="fa fa-search"></i>
-                        </span>
-                    </div>
-                </div>
-            </form>
-        </div>
     </div>
 
 
