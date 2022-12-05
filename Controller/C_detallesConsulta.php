d6444fb152f098b7a7e74ce758b480eddb616f5d
Jose Luis <ruyjoy@hotmail.com>
2022-12-05 10:52:28 -0300

    Carrito completo

--- Controller/C_detallesConsulta.php
+++ Controller/C_detallesConsulta.php
@@ -24,4 +24,44 @@ if ($id == '') {
     $foto = substr($fila['foto_con'], 3);
 }
 
+if (isset($_POST['btnAgregar'])) {
+
+    $nombre_con = $nombrecon;
+    $precio_con= $precio;
+    $cantidad = $_POST['can'];
+
+    if (!isset($_SESSION['carrito'])) {
+
+
+        $producto = array(
+            'nombre_producto' => $nombre_con,
+            'precio_producto' => $precio_con,
+            'cantidad_producto' => $cantidad
+        );
+        $_SESSION['carrito'][0] = $producto;
+    } else { //si existe el producto contamos cuántos hay en el carrito
+        // estoy haciendo las validaciones con nombre porque no me toma el id del producto 
+        $nom_con = array_column($_SESSION['carrito'], 'nombre_producto');
+        if (in_array($nombre_con, $nom_con)) {
+            echo "<script>alert('El producto ya está en el carrito');</script>";
+        } else {
+
+            $num_pcon = count($_SESSION['carrito']);
+            $producto = array(
+                'nombre_producto' => $nombre_con,
+                'precio_producto' => $precio_con,
+                'cantidad_producto' => $cantidad
+            );
+            $_SESSION['carrito'][$num_productos] = $producto;
+        }
+    }
+
+    echo "<script>window.location='mostrarCarrito.php'</script>";
+    
+
+
+
+}
+
+
 ?>
